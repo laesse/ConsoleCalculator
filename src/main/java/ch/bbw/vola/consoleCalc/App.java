0de440a56d7bb5de28a93535c00d290044c690fa
Lässe <lars.volkheimer@gmx.ch>
2018-11-16 08:22:11 +0100

    add app & appTest

---
+++ src/main/java/ch/bbw/vola/consoleCalc/App.java
@@ -0,0 +1,7 @@
+package ch.bbw.vola.consoleCalc;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
---
+++ src/test/java/ch/bbw/vola/consoleCalc/AppTest.java
@@ -0,0 +1,12 @@
+package ch.bbw.vola.consoleCalc;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+}
